e5a3b490974732b808665e47cd83d6fa359372cb
Luis Henrique <luishsilva09@gmail.com>
2024-10-20 15:59:52 -0300

    importação

---
+++ mains.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+import json
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from dotenv import load_dotenv
+import os
