eada924b4ab41d0e2ce3fe267ad5546a46dc96aa
Luis Henrique <luishsilva09@gmail.com>
2024-10-20 16:00:17 -0300

    configurar chamada api

--- mains.py
+++ mains.py
@@ -6,3 +6,14 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from dotenv import load_dotenv
 import os
+
+#  Configurar env
+load_dotenv()
+
+# Configurar chamadas para api coingecko
+headers = {
+    "x-cg-demo-api-key": os.getenv('API_KEY')
+}
+url_api = "https://api.coingecko.com/api/v3/"
+
+
